5e72dbfc6b0a8d4746134a4b3b9419b06d30f7e7
Mohamed Khaled Tounsi <tounsi.medkhaled@gmail.com>
2019-11-19 23:16:14 -0500

    first commit

---
+++ launchDarklyFeatureToggle.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace launchDarklyFeatureToggle.Client
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            Task.Delay(3000);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001/api/values");
+                var client = new HttpClient();
+                var result = await client.SendAsync(request);
+                if (result.IsSuccessStatusCode)
+                {
+                    var message = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);  
+                }
+              
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ launchDarklyFeatureToggle/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaunchDarkly.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace launchDarklyFeatureToggle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private LdClient ldClient;
+
+        public ValuesController()
+        {
+            ldClient = new LdClient("sdk-638d5200-59cb-46a3-aaaa-0d38f30fd913");
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<bool> Get()
+        {
+            
+            User user = LaunchDarkly.Client.User.WithKey("khaled");
+            user.FirstName = Guid.NewGuid().ToString();
+            bool showFeature = ldClient.BoolVariation("beta-feature", user, false);           
+            return showFeature;
+        }
+
+       
+    }
+}
